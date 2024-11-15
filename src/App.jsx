d58c10dd45b815210f9fbbcbc0ea0bfbb562aaea
Krystal304 <krystalwilliams864@gmail.com>
2024-11-15 16:14:38 -0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+  <div className="app">
+    <div className="main">main</div>
+      <div className="pyramid">money</div>
+       
+  </div>
+
+
+  )
+  
+}
+
+export default App
