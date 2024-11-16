612c98dc18d9ee5162426c86a72c59e94e73b3d1
Krystal304 <krystalwilliams864@gmail.com>
2024-11-15 21:16:59 -0500

    fix main and question container

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,22 @@ import './App.css'
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1)
   const [timeOut, setTimeOut] = useState(false)
-  return (
-  <div className="app">
-    <div className="main">main</div>
-      <Quiz
-      data={data}
-      questionNumber={questionNumber}
-      setQuestionNumber={setQuestionNumber}
-      setTimeOut={setTimeOut}
-      ></Quiz>
-      <div className="pyramid">money</div>
-       
-  </div>
-
-
-  )
   
+  return (
+    <div className="app">
+      <div className="main">
+        <Quiz
+          data={data}
+          questionNumber={questionNumber}
+          setQuestionNumber={setQuestionNumber}
+          setTimeOut={setTimeOut}
+        />
+      </div>
+      <div className="timer">Timer</div>
+      <div className="pyramid">money</div>
+    </div>
+  )
 }
 
 export default App
+
