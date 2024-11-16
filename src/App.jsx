2357e7163c74322a267971fe75017b2912969dc0
Krystal304 <krystalwilliams864@gmail.com>
2024-11-15 21:39:42 -0500

    prize money

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import Quiz from './components/Quiz'
 
 
 import './App.css'
-
 function App() {
-  const [questionNumber, setQuestionNumber] = useState(1)
-  const [timeOut, setTimeOut] = useState(false)
-  
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [timeOut, setTimeOut] = useState(false);
+
   return (
     <div className="app">
       <div className="main">
@@ -20,10 +19,20 @@ function App() {
         />
       </div>
       <div className="timer">Timer</div>
-      <div className="pyramid">money</div>
+      <div className="pyramid">
+        <ul>
+          {prizeMoney.map((item) => (
+            <li
+              key={item.id}
+              className={questionNumber === item.id ? "item active" : "item"}
+            >
+              <h5 className="amount">{item.amount}</h5>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
